73c4f5f3eb7080768aacbb822983fe40e8597700
Gouravspopale <gouravspopale111@gmail.com>
2023-03-14 16:48:14 +0530

    123

---
+++ G2.py
@@ -0,0 +1,28 @@
+# program to print Even and Odd number
+n=10
+if(n%2==0):
+    print("The Given Value Is Even",n)
+else:
+    print("The Given Value Is Odd",n)
+
+#program to find Positive and Negative values 
+n=7
+if(n>0):
+    print("The Given Value Is  Positive",n)
+else:
+     print("The Given Value Is Negative",n)   
+
+# program to find Simple intrest
+p,t,r=12000,4,3
+si=p*(t*r)/100
+print("SiMPLE INTREST",si)
+
+#program to find Simple intrest
+p,t,r=5000,2,5.4
+a=p*(1+(r/100))*t
+print("SiMPLE INTREST",a)
+
+# program to find Swap of number
+p,q=10,5
+p,q=q,p
+print("Swap of p",p)
