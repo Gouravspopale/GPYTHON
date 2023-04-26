c3c1471c64e4b85bdd6e63a0ae6c44f318556509
Gouravspopale <gouravspopale111@gmail.com>
2023-04-26 15:31:06 +0530

    CHANGES DONE

--- G1.py
+++ G1.py
@@ -1,27 +1,27 @@
-#  # python program to print message.
-# print(" HI GOURAV WELL COME TO PYTHON PROGRAM")
+ # python program to print message.
+print(" HI GOURAV WELL COME TO PYTHON PROGRAM")
 
-# #python program to print sum of two numbers
-# a,b=10,5
-# print("Sum Of Two numbers",a+b)
+#python program to print sum of two numbers
+a,b=10,5
+print("Sum Of Two numbers",a+b)
 
-# # python program to print area and perimeter of square
-# a=5 
-# area=(a*a)
-# peri=(a*4)
-# print("\nArea of Square",area,"\nperi of Square",peri)
+# python program to print area and perimeter of square
+a=5 
+area=(a*a)
+peri=(a*4)
+print("\nArea of Square",area,"\nperi of Square",peri)
 
-# #python program to print area and perimeter of Rectangle
-# l,b=5,3
-# area=(l*b)
-# peri=2*(l+b)
-# print("\nArea of Rectangle",area,"\nperi of Rectangle",peri)
+#python program to print area and perimeter of Rectangle
+l,b=5,3
+area=(l*b)
+peri=2*(l+b)
+print("\nArea of Rectangle",area,"\nperi of Rectangle",peri)
 
-# #python program to print area and perimeter of Circle
-# r=7
-# area=(7*7)
-# peri=(2*3.14*7)
-# print("\nArea of Circle",area,"\nperi of Circle",peri)
+#python program to print area and perimeter of Circle
+r=7
+area=(7*7)
+peri=(2*3.14*7)
+print("\nArea of Circle",area,"\nperi of Circle",peri)
 
 import tkinter as tk
 
